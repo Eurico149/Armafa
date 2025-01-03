81bed425212319f553a10ee2a391f0d336f38b5b
euric <euricogabriel149@gmail.com>
2025-01-03 02:37:01 -0300

    Produtos quase completo :)

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from view.App import App
+
+master = tk.Tk()
+root = App(master)
+
+root.mainloop()
+
---
+++ src/model/Produto.py
@@ -0,0 +1,17 @@
+class Produto:
+    def __init__(self, id_pro: int, nome, valor: int):
+        self.id_pro = id_pro
+        self.nome = nome
+        self.valor = valor
+
+    def __str__(self):
+        saida = " "
+        saida += "0" * (3 - len(str(self.id_pro))) + str(self.id_pro) + " | "
+        saida += str(self.nome) + ((36 - len(self.nome)) * " ") + "| "
+        saida += "R$" + f"{(self.valor/100):.2f}"
+        return saida
+
+    def __eq__(self, other):
+        if isinstance(other, Produto):
+            return (self.id_pro == other.id_pro) and (self.nome == other.nome) and (self.valor == other.valor)
+        return False
